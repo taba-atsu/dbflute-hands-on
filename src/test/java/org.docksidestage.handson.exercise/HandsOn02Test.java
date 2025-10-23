a47d1ef4f0c84422aa03e8259ee37a1fff934439
Atsushi Tabata <a.tabata@bizreach.co.jp>
2025-10-23 19:02:13 +0900

    section02事前準備

--- src/test/java/org.docksidestage.handson.exercise/HandsOn02Test.java
+++
@@ -1,6 +0,0 @@
-package org.docksidestage.handson.exercise;
-
-import org.docksidestage.handson.unit.UnitContainerTestCase;
-
-public class HandsOn02Test extends UnitContainerTestCase {
-}
---
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -0,0 +1,7 @@
+package org.docksidestage.handson.exercise;
+
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
+public class HandsOn02Test extends UnitContainerTestCase {
+}
+// 自分でディレクトリをコマンドで作成したらパッケージの設定がうまくいかない。パッケージを作成してから、その中にファイルを作成するとうまく作成できた。
