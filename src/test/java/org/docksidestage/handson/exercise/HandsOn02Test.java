ee2a7fe03564050457fcc2fedca937c469f3db58
Atsushi Tabata <a.tabata@bizreach.co.jp>
2025-10-27 08:39:57 +0900

    section02の途中

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -1,7 +1,22 @@
 package org.docksidestage.handson.exercise;
 
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 public class HandsOn02Test extends UnitContainerTestCase {
+
+    @Resource
+    private MemberBhv memberBhv;
+
+    public void test_existsTestData() throws Exception {
+        // ## Arrange ##
+        
+        
+        // ## Act ##
+    
+        // ## Assert ##
+    }
 }
 // 自分でディレクトリをコマンドで作成したらパッケージの設定がうまくいかない。パッケージを作成してから、その中にファイルを作成するとうまく作成できた。
