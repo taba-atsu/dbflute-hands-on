b8f14a8a865de381a4d75e0ad19769385babba48
taba-atsu <a.tabata@bizreach.co.jp>
2026-04-30 11:56:27 +0900

    03の4の途中まで

--- src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -7,6 +7,8 @@ import javax.annotation.Resource;
 
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.dbflute.exentity.MemberSecurity;
+import org.docksidestage.handson.dbflute.exbhv.MemberSecurityBhv;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 public class HandsOn03Test extends UnitContainerTestCase {
@@ -17,17 +19,80 @@ public class HandsOn03Test extends UnitContainerTestCase {
 
     @Resource
     private MemberBhv memberBhv;
+    private MemberSecurityBhv memberSecurityBhv;
     
     public void test_member_start_with_s_and_before_birth_19680101() throws Exception {
         // ## Arrange ##
         
         // ## Act ##
-        List<Member> memberList = memberBhv.selectList(cb ->{
+        List<Member> memberList = memberBhv.selectList(cb -> {
             cb.query().setMemberName_LikeSearch("S", op ->
                     op.likePrefix());
             cb.query().setBirthdate_LessEqual(LocalDate.of(1968,1,1));
         });
     
         // ## Assert ##
+        memberList.forEach(member ->{
+            LocalDate birthdate = member.getBirthdate();
+            log("memberBirthdate: {}", birthdate);
+            assertTrue(member.getMemberName().startsWith("S"));
+            assertTrue(birthdate.isBefore(LocalDate.of(1968, 1, 2)));
+        });
+    }
+
+    public void test_member_status_and_member_security_info() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        List<Member> memberList = memberBhv.selectList(cb -> {
+            cb.setupSelect_MemberStatus();
+            cb.setupSelect_MemberSecurityAsOne();
+            cb.query().addOrderBy_Birthdate_Asc().withNullsLast();
+            cb.query().addOrderBy_MemberId_Asc();
+        });
+
+        // ## Assert ##
+        memberList.forEach(member -> {
+            log("birthdate={}, memberId={}", member.getBirthdate(), member.getMemberId());
+            assertNotNull(member.getMemberStatus().get());
+            assertNotNull(member.getMemberSecurityAsOne().get());
+        });
+    }
+    
+    public void test_member_security_reminder_question() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        List<Member> memberList = memberBhv.selectList(cb -> {
+            cb.query().queryMemberSecurityAsOne()
+                    .setReminderQuestion_LikeSearch("2", op -> op.likeContain());
+        });
+
+        // ## Assert ##
+        memberList.forEach(member -> {
+            MemberSecurity security = memberSecurityBhv.selectEntityWithDeletedCheck(cb -> {
+                cb.query().setMemberId_Equal(member.getMemberId());
+            });
+            log("memberId={}, answer={}", member.getMemberId(), security.getReminderQuestion());
+            assertTrue(security.getReminderQuestion().contains("2"));
+        });
+        // N+1が発生してしまうのが気になるが、どうすればいいのか思いつかなかった、、
+        // DDLをみたら会員一人につき必ず一つのセキュリティ情報があるというコメントがあったので、AssertではselectEntityWithDeletedCheckを使用した
+    }
+    
+    public void test_member_status_order() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        List<Member> memberList = memberBhv.selectList(cb -> {
+            cb.query().queryMemberStatus().addOrderBy_DisplayOrder_Asc();
+            cb.query().addOrderBy_MemberId_Desc();
+        });
+    
+        // ## Assert ##
+//        memberList.forEach(member -> {
+//            log("memberId={}, displayOrder={}",member.getMemberId(),);
+//            assertNull(member.getMemberStatus());
+//        });
     }
 }
